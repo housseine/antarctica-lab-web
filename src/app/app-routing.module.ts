76c143aff2cf739083ac71d592acb8846e7aaf43
housseine <housseine2014tassa@gmail.com>
2019-12-16 09:24:48 +0100

    [feat] - cercular trombino

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule,Routes} from '@angular/router'
 import { GamesComponent } from './games/games.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GameDetailsComponent } from './game-details/game-details.component';
+import { GameCardComponent } from './game-card/game-card.component';
 
 const routes:Routes=[
   {path:'games',component:GamesComponent},
   {path:'dashboard',component:DashboardComponent},
   {path:'details/:id',component:GameDetailsComponent},
+  {path:'trombinogame',component:GameCardComponent},
   {path:'',redirectTo:'/dashboard',pathMatch:'full'},
 
 ];
--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GameService } from '../game.service';
+import { Game } from '../games/game';
+declare var $: any;
+declare const bubbles:any;
+//declare var bubbler;
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.css']
 })
 export class GameCardComponent implements OnInit {
-
-  constructor() { }
+  games: Game[];
+  constructor(public gameService: GameService) { }
 
   ngOnInit() {
+    this.getGames();
+   
   }
 
+  getGames(): void {
+    this.gameService.getGames().subscribe(games => this.games = games);
+  }
 }
