9c2b8a6df12c34b169f6ef787e4482eb7cd14d0c
housseine <housseine2014tassa@gmail.com>
2019-12-19 00:30:59 +0100

    [feat] - added d3v4 bubble chart

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../game.service';
 import { Game } from '../games/game';
-declare var $: any;
-declare const bubbles:any;
-declare function makeBubbles():any;
+import * as d3 from 'd3';
 
 
 @Component({
@@ -15,18 +13,80 @@ export class GameCardComponent implements OnInit {
   games: Game[];
 
   constructor(public gameService: GameService) {
-   
-}
-  ngOnInit() {
-   this.getGames();
-    setTimeout(() => {
-      //alert("works");
-      makeBubbles();
-    }, 300);
-  }
 
+  }
+  ngOnInit() {
+    this.getGames();
+    this.makechart();
+  }
+  ngAfterContentInit() {
+    // d3.select("p").style("color", "red");
+
+
+  }
 
   getGames(): void {
     this.gameService.getGames().subscribe(games => this.games = games);
   }
+  makechart() {
+
+
+    var diameter = 960,
+      format = d3.format(",d"),
+      color = d3.scaleOrdinal(d3.schemeAccent);
+
+    var bubble = d3.pack()
+      .size([diameter, diameter])
+      .padding(1.5);
+
+    var svg = d3.select("body").append("svg")
+      .attr("width", diameter)
+      .attr("height", diameter)
+      .attr("class", "bubble");
+
+      d3.json("assets/data.json").then(function ( data) {
+
+      var root = d3.hierarchy(classes(data))
+        .sum(function (d) { return d.value; })
+        .sort(function (a, b) { return b.value - a.value; });
+
+      bubble(root);
+      var node = svg.selectAll(".node")
+        .data(root.children)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+      node.append("title")
+        .text(function (d) { return d.data.className + ": " + format(d.value); });
+
+      node.append("circle")
+        .attr("r", function (d) { return d.r; })
+        .style("fill", function (d) {
+          return color(d.data.packageName);
+        });
+
+      node.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function (d) { return d.data.className.substring(0, d.r / 3); });
+    });
+
+    // Returns a flattened hierarchy containing all leaf nodes under the root.
+    function classes(root) {
+      var classes = [];
+
+      function recurse(name, node) {
+        if (node.children) node.children.forEach(function (child) { recurse(node.name, child); });
+        else classes.push({ packageName: name, className: node.name, value: node.size });
+      }
+
+      recurse(null, root);
+      return { children: classes };
+    }
+
+    d3.select(self.frameElement).style("height", diameter + "px");
+
+  }
+
 }
