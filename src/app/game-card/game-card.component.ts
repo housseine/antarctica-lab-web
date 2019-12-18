ebc5112b6e59005e73bb64b02802f0069cb775c7
housseine <housseine2014tassa@gmail.com>
2019-12-18 22:12:57 +0100

    [feat] animated trombinoscope

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -3,20 +3,28 @@ import { GameService } from '../game.service';
 import { Game } from '../games/game';
 declare var $: any;
 declare const bubbles:any;
-//declare var bubbler;
+declare function makeBubbles():any;
+
+
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
-  styleUrls: ['./game-card.component.css']
+  styleUrls: ['./game-card.component.scss']
 })
 export class GameCardComponent implements OnInit {
   games: Game[];
-  constructor(public gameService: GameService) { }
 
-  ngOnInit() {
-    this.getGames();
+  constructor(public gameService: GameService) {
    
+}
+  ngOnInit() {
+   this.getGames();
+    setTimeout(() => {
+      //alert("works");
+      makeBubbles();
+    }, 300);
   }
+
 
   getGames(): void {
     this.gameService.getGames().subscribe(games => this.games = games);
