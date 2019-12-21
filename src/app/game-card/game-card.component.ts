a16e09fe0f7b033f3d468bf335a67f998ec9cd61
housseine <housseine2014tassa@gmail.com>
2019-12-21 22:00:10 +0100

    [feat] - added navigation in bubble chart

--- src/app/game-card/game-card.component.ts
+++ src/app/game-card/game-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameService } from '../game.service';
 import { Game } from '../games/game';
 import * as d3 from 'd3';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,12 +13,12 @@ import * as d3 from 'd3';
 export class GameCardComponent implements OnInit {
   games: Game[];
 
-  constructor(public gameService: GameService) {
+  constructor(public gameService: GameService,private router:Router) {
 
   }
   ngOnInit() {
     this.getGames();
-    this.makechart();
+
   }
   ngAfterContentInit() {
     // d3.select("p").style("color", "red");
@@ -26,9 +27,9 @@ export class GameCardComponent implements OnInit {
   }
 
   getGames(): void {
-    this.gameService.getGames().subscribe(games => this.games = games);
+    this.gameService.getGames().subscribe(games => { this.games = games; this.makechart(games,this.router); });
   }
-  makechart() {
+  makechart(data,router) {
 
 
     var diameter = 960,
@@ -39,38 +40,44 @@ export class GameCardComponent implements OnInit {
       .size([diameter, diameter])
       .padding(1.5);
 
-    var svg = d3.select("body").append("svg")
+    var svg = d3.select("app-game-card").append("svg")
       .attr("width", diameter)
       .attr("height", diameter)
       .attr("class", "bubble");
 
-      d3.json("assets/data.json").then(function ( data) {
+    data = fromJsonToDsJson(data);
+   
+    var root = d3.hierarchy(classes(data))
+      .sum(function (d) { return d.value; })
+      .sort(function (a, b) { return b.value - a.value; });
+    bubble(root);
+    var node = svg.selectAll(".node")
+      .data(root.children)
+      .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-      var root = d3.hierarchy(classes(data))
-        .sum(function (d) { return d.value; })
-        .sort(function (a, b) { return b.value - a.value; });
+    node.append("title")
+      .text(function (d) { return d.data.className + ": " + format(d.value); });
 
-      bubble(root);
-      var node = svg.selectAll(".node")
-        .data(root.children)
-        .enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-      node.append("title")
-        .text(function (d) { return d.data.className + ": " + format(d.value); });
-
-      node.append("circle")
-        .attr("r", function (d) { return d.r; })
-        .style("fill", function (d) {
-          return color(d.data.packageName);
-        });
+    node.append("circle")
+      .on("click", function (d) {router.navigate(['details/'+d.data.id]) })
+      .attr("r", function (d) { return d.r; })
+      .style("fill", function (d) {
+        return color(d.data.value);
+      });
 
       node.append("text")
-        .attr("dy", ".3em")
-        .style("text-anchor", "middle")
-        .text(function (d) { return d.data.className.substring(0, d.r / 3); });
-    });
+      .on("click", function (d) {router.navigate(['details/'+d.data.id]) })
+      .attr("dy", ".3em")
+      .style("text-anchor", "middle")
+      .text(function (d) { return d.data.className.substring(0, d.r / 3); });
+
+    // node.append("a")
+    //   .attr("href", "/details/3")
+    //   .attr("dy", ".3em")
+    //   .style("text-anchor", "middle")
+    //   .text(function (d) { return d.data.className.substring(0, d.r / 3); });
 
     // Returns a flattened hierarchy containing all leaf nodes under the root.
     function classes(root) {
@@ -78,7 +85,7 @@ export class GameCardComponent implements OnInit {
 
       function recurse(name, node) {
         if (node.children) node.children.forEach(function (child) { recurse(node.name, child); });
-        else classes.push({ packageName: name, className: node.name, value: node.size });
+        else classes.push({ packageName: name, className: node.name, value: node.rate,id: node.id });
       }
 
       recurse(null, root);
@@ -86,6 +93,12 @@ export class GameCardComponent implements OnInit {
     }
 
     d3.select(self.frameElement).style("height", diameter + "px");
+
+    function fromJsonToDsJson(data) {
+      var dataJson = { "children": data };
+      return dataJson;
+
+    }
 
   }
 
