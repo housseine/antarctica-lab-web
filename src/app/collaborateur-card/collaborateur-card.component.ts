1fbbdc48790e03a633c13b8a8a8adec6f0cda128
Housseine TASSA <C62228@meaa.net.intra>
2019-12-25 17:26:39 +0100

    [feat] - adde images to trombinoscope

--- src/app/collaborateur-card/collaborateur-card.component.ts
+++ src/app/collaborateur-card/collaborateur-card.component.ts
@@ -38,13 +38,13 @@ export class CollaborateurCardComponent implements OnInit {
   }
 
   makechart(collaborateurs, router) {
-    var diameter = 960,
+    var diameter = 1000,
       format = d3.format(",d"),
       color = d3.scaleOrdinal(d3.schemeAccent);
 
     var bubble = d3.pack()
-      .size([diameter, diameter])
-      .padding(1.5);
+      .size([diameter, diameter]);
+    // .padding(1.5)
 
     d3.select("svg").remove();
 
@@ -55,46 +55,64 @@ export class CollaborateurCardComponent implements OnInit {
 
     var data: any;
     data = transformJsonToD3Json(collaborateurs);
+    // d3.json("assets/mockdata.json").then((data) => {
+      var root = d3.hierarchy(classes(data))
+        .sum(function (d: any) { return d.value; });
+      // .sort(function (a, b) { return b.value - a.value; })
+      //console.log(root.children.length);
+      bubble(root);
 
-    var root = d3.hierarchy(classes(data))
-      .sum(function (d: any) { return d.value; })
-      .sort(function (a, b) { return b.value - a.value; });
 
-    bubble(root);
+      var node = svg.selectAll(".node")
+        .data(root.children)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d: any) { return "translate(" + d.x + 1 + "," + d.y + 1 + ")"; })
+        .style('cursor', 'pointer');
 
-    var node = svg.selectAll(".node")
-      .data(root.children)
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function (d: any) { return "translate(" + d.x + "," + d.y + ")"; })
-      .style('cursor', 'pointer');
+        var img_id = function(d:any){ console.log("hello"+d.data.id);return "img_" + d.data.id; }
+        var img_url = function(d:any){ return "url(#img_" + d.data.id + ")"; }
+        var defs = node.append("defs")
+        var imgPattern = defs
+          .append("pattern")
+          .attr("id", img_id)
+          .attr("width", 1)
+          .attr("height", 1)
+          .attr("patternUnits", "objectBoundingBox")
+          .append("image")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", function(d:any){ return d.r*2;})
+          .attr("height", function(d:any){ return d.r*2;})
+          .attr("xlink:href", function (d:any) {
+          return "https://randomuser.me/api/portraits/women/"+d.data.id+".jpg" ;
+          });
 
- 
 
-    node.append("title")
-      .text(function (d: any) { return d.data.className + ": " + format(d.value); });
+      node.append("title")
+        .text(function (d: any) { return d.data.className + ": " + format(d.value / 2); });
 
-    node.append("circle")
-      .on("click", function (d: any) { router.navigate(['details/' + d.data.id]) })
-      .attr("r", function (d: any) { return d.r; })
-      .style("fill", function (d: any) {
-        return color(Math.random().toString());
-      });
+      node.append("circle")
+        .on("click", function (d: any) { router.navigate(['details/' + d.data.id]) })
+        .attr("r", function (d: any) { return d.r; })
+        .style("fill", img_url);
       // var images = node.append("svg:image")
-      // .attr("xlink:href", function (d:any) {console.log(d.data.id); return "https://randomuser.me/api/portraits/thumb/men/"+d.data.id+".jpg";  })
-      // .attr("x", function (d) { return -25; })
-      // .attr("y", function (d) { return -25; })
+      // .attr("xlink:href", function (d:any) { return "http://pictify.saatchigallery.com/files/works/fluffy-bird-1465173013_b.jpg";  })
+      // .attr("x", function (d) { return 0; })
+      // .attr("y", function (d) { return 0; })
+      // .style('border-radisus','50%')
       // .attr("height", function(d:any){return d.r;})
       // .attr("width", function(d:any){return d.r;});
 
-    node.append("text")
-      .on("click", function (d: any) { router.navigate(['details/' + d.data.id]) })
-      .attr("dy", ".3em")
-      .style("text-anchor", "middle")
-      .text(function (d: any) { return d.data.className.substring(0, d.r / 3); });
+      // node.append("text")
+      //   .on("click", function (d: any) { router.navigate(['details/' + d.data.id]) })
+      //   .attr("dy", ".3em")
+      //   .style("text-anchor", "middle")
+      //   .style('font-size', '100%')
+      //   .text(function (d: any) { return d.data.className.substring(0, d.r / 3); });
 
-    svg.append("style").text(".bubble{margin:auto;display:block;}g circle:hover {fill:gray !important;}g:hover {fill:blue !important;} ");
-
+      svg.append("style").text(".bubble{margin:auto;display:block;}g circle:hover {fill:gray !important;}g:hover {fill:blue !important;} ");
+        
     function classes(root) {
       var classes = [];
 
@@ -116,9 +134,6 @@ export class CollaborateurCardComponent implements OnInit {
     }
     d3.select(self.frameElement).style("height", diameter + "px");
   }
-
-
-
 }
 
 
