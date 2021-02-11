681c3eeb97479e7c2cee1f519658b8577b5695cf
housseine <housseine2014tassa@gmail.com>
2021-02-11 22:57:05 +0100

    [FEAT] - Added product feature

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,32 +2,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { GlobalService } from '../common/services/global.service';
-import { Clothes } from '../DTO/Clothes';
+import { ProductColorSizes } from '../DTO/ProductColorSizes';
 const BASE_API_URL = environment.ENDPOINT_ROOT_URL;
 @Injectable({
   providedIn: 'root'
 })
-export class ClothesService {
-  private productUrl = BASE_API_URL + '/public/product';
+export class ProductService {
+  private productUrl = BASE_API_URL + '/public/product/spec';
   constructor(private globalService: GlobalService) { }
-  
-  getClothes(): Observable<Clothes[]> {
+
+  getProductSpecs(): Observable<ProductColorSizes[]> {
     return this.globalService.getAllObjects(this.productUrl);
   }
-  getClothesById(id: number): Observable<Clothes> {
+  getProductSpecById(id: number): Observable<ProductColorSizes> {
     return this.globalService.getObjectById(this.productUrl, id);
   }
-  updateClothes(clothes: Clothes): Observable<any> {
+  updateProductSpec(clothes: ProductColorSizes): Observable<any> {
     return this.globalService.updateObject(this.productUrl, clothes);
   }
-  addClothes(clothes: Clothes): Observable<Clothes> {
+  addProductSpec(clothes: ProductColorSizes): Observable<ProductColorSizes> {
     return this.globalService.addObject(this.productUrl, clothes);
   }
-  deleteClothes(clothes: Clothes): Observable<any> {
+  deleteProductSpec(clothes: ProductColorSizes): Observable<any> {
     return this.globalService.deleteObject(this.productUrl, clothes);
   }
-  searchClothes(term: string): Observable<Clothes[]> {
+  searchProductSpec(term: string): Observable<ProductColorSizes[]> {
     return this.globalService.searchObjectsByTerm(this.productUrl, term);
   }
-
 }
